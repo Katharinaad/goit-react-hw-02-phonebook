83b50ae01f11dd9c17583d189dca085fe67aac61
Katharinaad <vermilion.adams@gmail.com>
2023-10-11 12:26:10 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ export class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
   };
 
   handleChange = event => {
@@ -32,10 +33,20 @@ export class App extends Component {
   render() {
     const { contacts, name } = this.state;
     return (
-      <>
+      <div
+        style={{
+          maxWidth: '400px',
+        }}
+      >
         <h2>Phonebook</h2>
         <form onSubmit={this.handleSubmit}>
-          <label>
+          <label
+            style={{
+              display: 'flex',
+              gap: '10px',
+              marginBottom: '10px',
+            }}
+          >
             Name
             <input
               type="text"
@@ -48,6 +59,15 @@ export class App extends Component {
               required
             />
           </label>
+          <label style={{}}>Number</label>
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            placeholder="Enter a number"
+            required
+          />
           <button type="submit">Add contact</button>
         </form>
         <h2>Contacts</h2>
@@ -56,7 +76,7 @@ export class App extends Component {
             <li key={contact.id}>{contact.name}</li>
           ))}
         </ul>
-      </>
+      </div>
     );
   }
 }
