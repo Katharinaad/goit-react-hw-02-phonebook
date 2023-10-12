996255d07024ab8b7278cfbfa1be3632c35cb4d4
Katharinaad <vermilion.adams@gmail.com>
2023-10-12 21:26:22 +0200

    Update ContactListItem.jsx

--- src/components/ContactList/ContactListItem.jsx
+++ src/components/ContactList/ContactListItem.jsx
@@ -1,12 +1,17 @@
+import css from './ContactListItem.module.css';
+
 export function ContactListItem({ contact, handleDelete }) {
   return (
-    <>
+    <div className={css.contactList}>
       <li>
         {contact.name}: {contact.number}
       </li>
-      <button className="buttonDelete" onClick={() => handleDelete(contact.id)}>
+      <button
+        className={css.buttonDelete}
+        onClick={() => handleDelete(contact.id)}
+      >
         Delete
       </button>
-    </>
+    </div>
   );
 }
