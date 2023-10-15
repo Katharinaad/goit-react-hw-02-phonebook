56c39a3da92ed3c228d0ae955f32be020a1b3844
Katharinaad <vermilion.adams@gmail.com>
2023-10-15 19:09:23 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,8 +1,9 @@
 import { ContactListItem } from './ContactListItem';
+import css from './ContactList.module.css';
 
 export function ContactList({ filteredContacts, handleDelete }) {
   return (
-    <ul>
+    <ul className={css.contactList}>
       {filteredContacts.map(contact => (
         <ContactListItem
           key={contact.id}
